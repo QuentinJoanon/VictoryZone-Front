d51ff941a70199f06617b9695f94bd1c089cf00f
axel-martin7 <axel.martin@outlook.com>
2023-06-16 17:28:43 +0200

    fix: recreate CardFutureEvents html structure

--- src/app/components/CardFutureEvents.tsx
+++ src/app/components/CardFutureEvents.tsx
@@ -15,13 +15,23 @@ export default function CardFutureEvents({
   return (
     <div className="event">
       <h2 className="event__name">{event_name}</h2>
-      <h3 className="event__date">event_date</h3>
+      <h3 className="event__date">{event_date}</h3>
       <div className="event__teams">
-        <div className="event__teams__home-logo"></div>
+        <div className="event__teams__home">
+          <p className="event__teams__home__short-name">VZ</p>
+          <div className="event__teams__home__logo"></div>
+        </div>
         <p>Vs</p>
-        <div className="event__teams__away-logo"></div>
+        <div className="event__teams__away">
+          <p className="event__teams__away__short-name">
+            {adversary_name_short}
+          </p>
+          <div className="event__teams__logo__away"></div>
+        </div>
       </div>
-      <button type="button">live_link</button>
+      <button className="event__button" type="button">
+        {live_link}
+      </button>
     </div>
   );
 }
