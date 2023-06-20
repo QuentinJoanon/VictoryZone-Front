e755dc4437efe56b1ebb085de1befee90514e554
QuentinJoanon <quentin.joanon@gmail.com>
2023-06-20 21:57:20 +0200

    feat: delete article missing refresh

---
+++ src/app/components/DeleteArticleModal.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { deleteArticle } from '../components_api/ArticlesAdmin';
+import { useRouter } from 'next/navigation';
+
+export default function DeleteArticleModal({ slug, closeModal }) {
+  const router = useRouter();
+  const handleDeleteSubmit = (e, slug) => {
+    e.preventDefault();
+
+    deleteArticle(slug);
+
+    // Fermer la modal après la suppression
+    closeModal();
+
+    router.refresh();
+  };
+  return (
+    <div className="modal-box">
+      <h3>Voulez-vous vraiment supprimer cet article ?</h3>
+      <form
+        className="modal-action"
+        onSubmit={(e) => handleDeleteSubmit(e, slug)}
+      >
+        <button type="button" className="btn" onClick={closeModal}>
+          Fermer
+        </button>
+        <button type="submit" className="btn">
+          Supprimer
+        </button>
+      </form>
+    </div>
+  );
+}
