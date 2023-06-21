0258acf4bd9445fe178c39b99283afbf4b4633e7
axel-martin7 <axel.martin@outlook.com>
2023-06-21 13:57:52 +0200

    feat: Add recruitment page + html structure and infos

---
+++ src/app/recrutement/page.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/no-unescaped-entities */
+export default function Cgu() {
+  return (
+    <main>
+      <h1>RECRUTEMENT</h1>
+      <div className="recrutement">
+        <p className="recrutement__description">
+          Bienvenue sur la page de recrutement de la Team VictoryZone. Si vous
+          êtes un joueur talentueux, passionné par League of Legends et prêt à
+          repousser vos limites, vous êtes au bon endroit. Rejoignez-nous dans
+          notre quête de l'excellence compétitive et de la domination sur la
+          scène de l'esport.
+        </p>
+      </div>
+    </main>
+  );
+}
