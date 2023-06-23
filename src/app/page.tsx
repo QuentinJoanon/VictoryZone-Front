08c035e9be06a306effc8100e2d4ba9f95aa9756
QuentinJoanon <quentin.joanon@gmail.com>
2023-06-23 15:54:15 +0200

    fix: route team home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 'use client';
 
-
-import Slider from "react-slick";
-import { settings } from './components/Carousel'; 
+import Slider from 'react-slick';
+import { settings } from './components/Carousel';
 import { useEffect } from 'react';
 import { AllArticles, fetchHomeArticles } from './components_api/ArticlesList';
 import {
@@ -10,7 +9,7 @@ import {
   AllPastCalendars,
   fetchHomeCalendars,
 } from './components_api/CalendarList';
-import { AllMembersTeam, fetchTeam } from './components_api/TeamList';
+import { AllMembersTeam, fetchTeamHome } from './components_api/TeamList';
 
 import { useArticleContext } from './context/Article';
 import { useCalendarContext } from './context/Calendar';
@@ -25,7 +24,7 @@ export default function Home() {
   useEffect(() => {
     fetchHomeArticles(setArticlesList);
     fetchHomeCalendars(setCalendarFutureList, setCalendarPastList);
-    fetchTeam(setTeamList);
+    fetchTeamHome(setTeamList);
   }, [
     setArticlesList,
     setCalendarFutureList,
@@ -40,16 +39,16 @@ export default function Home() {
     <main>
       <h1>Accueil</h1>
       <div style={{ maxWidth: '90%', margin: '0 auto' }}>
-      <Slider {...settings} className= "carousel">
-        {team.map((teamMember, index) => (
-          <div key={index} className="team__home">
-            {teamMember}
-          </div>
-        ))}
-      </Slider>
+        <Slider {...settings} className="carousel">
+          {team.map((teamMember, index) => (
+            <div key={index} className="team__home">
+              {teamMember}
+            </div>
+          ))}
+        </Slider>
       </div>
       <div>{calendars}</div>
-      <div className= "articles__home" >{articles}</div>
+      <div className="articles__home">{articles}</div>
     </main>
   );
 }
