5c1e0f3925866c45fc2dc3e4db980922984c3152
Axel Martin <axel.martin@outlook.com>
2023-07-26 16:31:31 +0200

    feat: Header and footer restyle + change rules on reset.scss

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,70 +1,131 @@
 'use client';
 
-import { useState } from 'react';
-import Image from 'next/image';
-import logo from '../../assets/logo.webp';
+import { useEffect, useState } from 'react';
 
 export default function Header() {
-  const [navOpen, setNavOpen] = useState(false);
-  const handleToggle = () => setNavOpen(!navOpen);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isDesktop, setIsDesktop] = useState(false);
+
+  // * Pour version desktop :
+  useEffect(() => {
+    const handleResize = () => {
+      const isDesktop = window.innerWidth >= 1200; //                 -- On verifie que la largeur de la fenetre est égale ou supérieure au breakpoint
+      setIsDesktop(isDesktop);
+      if (isDesktop) {
+        //                                                            -- On ferme le menu burger si on passe en version desktop
+        setIsMenuOpen(false);
+      }
+    };
+
+    window.addEventListener('resize', handleResize); //               -- On écoute les changements de la taille de la fenetre
+    handleResize(); //                                                -- On verifie la taille de la fenetre lors du chargement initial de la page
+    return () => {
+      //                                                              -- Nettoyage de l'ecouteur d'evenement lors du démontage du composant
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const handleBurgerMenuClick = () => {
+    //                                                                -- Fonction executée lors du clic sur le menu burger
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
     <header>
-   
-      <div className="header-container">
-        <a href="/">
-          <Image
-            src={logo}
-            height={100}
-            alt="Logo VictoryZone"
-            priority={true}
-          />
+      <div className="neon"></div>
+      <div className="header">
+        <div className="header__empty"></div>
+
+        <a className="header__title custom-font" href="/">
+          VictoryZone
         </a>
-        <div
-          className={`burger-menu ${navOpen ? 'active cross' : ''}`}
-          onClick={handleToggle}
-        >
-          <span className="line"></span>
-          <span className="line"></span>
-          <span className="line"></span>
+
+        {isDesktop ? (
+          <nav className="desktop-nav">
+            <ul className="desktop-nav__nav-list">
+              <li className="desktop-nav__list-item">
+                <a className="desktop-nav__list-item__link" href="/">
+                  Accueil
+                </a>
+              </li>
+              <li className="desktop-nav__list-item">
+                <a className="desktop-nav__list-item__link" href="/articles">
+                  Articles
+                </a>
+              </li>
+              <li className="desktop-nav__list-item">
+                <a className="desktop-nav__list-item__link" href="/team">
+                  Team
+                </a>
+              </li>
+              <li className="desktop-nav__list-item">
+                <a className="desktop-nav__list-item__link" href="/calendrier">
+                  Calendrier
+                </a>
+              </li>
+              <li className="desktop-nav__list-item">
+                <a className="desktop-nav__list-item__link" href="/recrutement">
+                  Recrutement
+                </a>
+              </li>
+            </ul>
+          </nav>
+        ) : (
+          <div
+            className={
+              isMenuOpen
+                ? 'header__burger-menu header__burger-menu--open'
+                : 'header__burger-menu'
+            }
+            onClick={handleBurgerMenuClick}
+          >
+            <span className="header__burger-menu-line"></span>
+            <span className="header__burger-menu-line"></span>
+            <span className="header__burger-menu-line"></span>
+          </div>
+        )}
+      </div>
+
+      {isMenuOpen && (
+        <div className="modal">
+          <ul className="modal__navigation-list">
+            <li className="modal__navigation-list__item">
+              <a className="modal__navigation-list__item__link" href="/">
+                Accueil
+              </a>
+            </li>
+            <li className="modal__navigation-list__item">
+              <a
+                className="modal__navigation-list__item__link"
+                href="/articles"
+              >
+                Articles
+              </a>
+            </li>
+            <li className="modal__navigation-list__item">
+              <a className="modal__navigation-list__item__link" href="/team">
+                Team
+              </a>
+            </li>
+            <li className="modal__navigation-list__item">
+              <a
+                className="modal__navigation-list__item__link"
+                href="/calendrier"
+              >
+                Calendrier
+              </a>
+            </li>
+            <li className="modal__navigation-list__item">
+              <a
+                className="modal__navigation-list__item__link"
+                href="/recrutement"
+              >
+                Recrutement
+              </a>
+            </li>
+          </ul>
         </div>
-      </div>
-      <div className={`header-menu ${navOpen ? '' : 'hidden'} `}>
-        <ul>
-          <li>
-            <a className="header-link" href="/">
-              Accueil
-            </a>
-          </li>
-          <li>
-            <a className="header-link" href="/team">
-              Team
-            </a>
-          </li>
-          <li>
-            <a className="header-link" href="/articles">
-              Articles
-            </a>
-          </li>
-          <li>
-            <a className="header-link" href="/calendrier">
-              Calendrier
-            </a>
-          </li>
-          <li>
-            <a className="header-link" href="/recrutement">
-              Recrutement
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div className="custom-shape-divider-top-1687786405">
-    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-        <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25" className="shape-fill1"></path>
-        <path d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z" opacity=".5" className="shape-fill2"></path>
-        <path d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z" className="shape-fill3"></path>
-    </svg>
-</div>
+      )}
     </header>
   );
 }
